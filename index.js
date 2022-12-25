5db0e2fed5988f2c08ee4758d541360edf82ee79
Kaspar Siricenko <kaspar.siricenko@gmail.com>
2022-12-25 14:25:42 +0000

    removed error handling

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.use(express.static('build'))
 
 app.use(require('./routes/getProducts'))
 
-app.use(function (req, res, next) {
-  next(createError(404))
-})
+// app.use(function (req, res, next) {
+//   next(createError(404))
+// })
 
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500).end()
-})
+// app.use(function (err, req, res, next) {
+//   res.status(err.status || 500).end()
+// })
 
 app.listen(5000, () => console.log('http://localhost:5000'))
 
